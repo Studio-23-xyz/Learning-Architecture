38f12181c00ae7f9e5ee8bbfa058ce163e29e7d7
Shahadat Hossain Shamim <shamim131666@gmail.com>
2021-11-16 18:09:26 +0600

    SoundAdded
    Background, Button, Car, Crash, Win

---
+++ Assets/Scripts/Sandbox/Shamim/CarSound.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class CarSound : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+    public AudioClip carSound;
+    public AudioClip crashSound;
+    public AudioClip winSound;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnEnable()
+    {
+        audioSource.clip = carSound;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("WinArea"))
+        {
+            audioSource.Stop();
+            audioSource.clip = winSound;
+            audioSource.loop = false;
+            audioSource.Play();
+        }
+
+        if (other.gameObject.CompareTag("LostArea"))
+        {
+            audioSource.Stop();
+            audioSource.clip = crashSound;
+            audioSource.loop = false;
+            audioSource.Play();
+        }
+    }
+}
